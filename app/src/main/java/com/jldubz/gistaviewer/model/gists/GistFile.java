824e487cd4049fd57d1ae479b9eee8ac174c4afd
Blackscure <Wekesabuyahi@gmail.com>
2020-06-13 14:26:17 +0300

    property created ,and geter and setter method created

--- app/src/main/java/com/jldubz/gistaviewer/model/gists/GistFile.java
+++ app/src/main/java/com/jldubz/gistaviewer/model/gists/GistFile.java
@@ -8,5 +8,13 @@ public class GistFile {
     }
     */
 
+    private String filename;
 
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
 }
