b5cbea09cb6edd694a84a3531efede299c615e5a
Blackscure <Wekesabuyahi@gmail.com>
2020-06-13 14:13:27 +0300

    properties ,get and setter methods created

--- app/src/main/java/com/jldubz/gistaviewer/model/gists/Gist.java
+++ app/src/main/java/com/jldubz/gistaviewer/model/gists/Gist.java
@@ -1,5 +1,10 @@
 package com.jldubz.gistaviewer.model.gists;
 
+import com.jldubz.gistaviewer.model.GitHubUser;
+
+import java.util.Date;
+import java.util.Map;
+
 public class Gist {
 
     /*
@@ -20,5 +25,58 @@ public class Gist {
       }
     */
 
+    private String id;
+    private Map<String, GistFile> files;
+    private Date created_at;
+    private  Date updated_at;
+    private String description;
+    private GitHubUser owner;
 
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public GitHubUser getOwner() {
+        return owner;
+    }
+
+    public void setOwner(GitHubUser owner) {
+        this.owner = owner;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public Map<String, GistFile> getFiles() {
+        return files;
+    }
+
+    public void setFiles(Map<String, GistFile> files) {
+        this.files = files;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
